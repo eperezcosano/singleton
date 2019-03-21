673feb5576cad91c088b0f9886ae7fc33e8fa125
Izan Pérez Cosano <izanperez98@gmail.com>
2019-03-21 16:32:08 +0100

    added test and modified class

--- src/main/java/dsa/eetac/upc/edu/I18NManager.java
+++ src/main/java/dsa/eetac/upc/edu/I18NManager.java
@@ -1,19 +1,29 @@
 package dsa.eetac.upc.edu;
 
 import java.util.HashMap;
+import java.util.ResourceBundle;
 
 class I18NManager {
-    private static I18NManager single_instance =null;
-    public HashMap h;
+    private static I18NManager singleInstance =null;
+    public HashMap<String, ResourceBundle> h;
 
     private I18NManager(){
-
+        h = new HashMap<String, ResourceBundle>();
     }
 
     public static I18NManager getInstance(){
-        if (single_instance == null) single_instance = new I18NManager();
-        return single_instance;
+        if (singleInstance == null) singleInstance = new I18NManager();
+        return singleInstance;
 
+    }
+
+    public String getText(String lang, String key) {
+        ResourceBundle rs =h.get(lang);
+        if (rs == null) {
+            rs = ResourceBundle.getBundle("dsa.eetac.upc.edu."+lang);
+            h.put(lang, rs);
+        }
+        return rs.getString(key);
     }
 
 }

2da44903593262651eea56c589dc34b5d98e68db
Izan Pérez Cosano <izanperez98@gmail.com>
2019-03-22 15:28:20 +0100

    getTextTest method

--- src/test/java/I18NManagerTest.java
+++ src/test/java/I18NManagerTest.java
@@ -1,26 +1,14 @@
-
 import dsa.eetac.upc.edu.I18NManager;
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Assert;
 
 
 public class I18NManagerTest {
 
-
-    @Before
-    public void initialize() {
-    }
-
-    @After
-    public void tearDown () {
-
-    }
-
     @Test
-    public void test() {
-
-        assertEquals("Hola", I18NManager.getInstance().getText("es", "l1"));
+    public void getTextTest () {
+        I18NManager im = I18NManager.getInstance();
+        Assert.assertEquals("Hola", im.getText("ca", "l1"));
     }
 }
